b04d6ccaf1501aa7e58f2c2dd54f1d9fa73e01d2
Pradish <pradishvaidyas@gmail.com>
2021-06-02 15:40:22 +0530

    six

--- pradish/calc/views.py
+++ pradish/calc/views.py
@@ -9,5 +9,5 @@ def add(request):
     val1=int(request.POST['num1'])
     val2=int(request.POST['num2'])
     res=val1+val2
-    return render(request,"result.html",{'result':res})
+    return render(request,"result.html",{'Result':res})
     
