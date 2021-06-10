d111f9eb23b60c0db26296892e2b50380e2b378c
Pradish <pradishvaidyas@gmail.com>
2021-06-10 21:39:28 +0530

    edited frontend

--- pradish/travello/models.py
+++ pradish/travello/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+#Mumbai
+class Destination():
+    id:int
+    name:str
+    price:int
+    dec:str
+    img:str
+
+#Chennai
+class Destination2():
+    id:int
+    name:str
+    price:int
+    dec:str
+    img:str
+
+#SDelhi
+class Destination3():
+    id:int
+    name:str
+    price:int
+    dec:str
+    img:str    
--- pradish/travello/views.py
+++ pradish/travello/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-
+from .models import Destination,Destination2,Destination3
 # Create your views here.
+
 def index(request):
-    return render(request,'index.html')
+    dest1=Destination()
+    dest1.name  = 'Mumbai'
+    dest1.price = 1000
+    dest1.desc  = 'The City In The North and The most famous city in india'
+    dest1.img   = 'destination_1.jpg'
+    dest2=Destination2()
+    dest2.name='Chennai'
+    dest2.price=5000
+    dest2.desc='The Modern City and the capital of Tamil Nadu'
+    dest2.img   = 'destination_2.jpg'
+
+    dest3=Destination3()
+    dest3.name='Delhi'
+    dest3.price=3000
+    dest3.desc='Capital city of India and the home of the president of India'
+    dest3.img   = 'destination_3.jpg'
+    dests=[dest1,dest2,dest3]
+    return render(request,'index.html',{'dests':dests})
