b23c44cd040eba22b77742a9e51f184b0712505e
Pradish <pradishvaidyas@gmail.com>
2021-06-10 22:21:05 +0530

    dest info edited

--- pradish/travello/models.py
+++ pradish/travello/models.py
@@ -2,26 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-#Mumbai
+#Destination
 class Destination():
     id:int
     name:str
     price:int
     dec:str
     img:str
-
-#Chennai
-class Destination2():
-    id:int
-    name:str
-    price:int
-    dec:str
-    img:str
-
-#SDelhi
-class Destination3():
-    id:int
-    name:str
-    price:int
-    dec:str
-    img:str    
+    offer=bool
+   
