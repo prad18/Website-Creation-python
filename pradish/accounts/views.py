6d09b27c86445b7aefd76129600d2de598b35f1f
Pradish <pradishvaidyas@gmail.com>
2021-06-14 19:43:08 +0530

    Commit test

--- pradish/accounts/views.py
+++ pradish/accounts/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import *
 
 # Create your views here.
-def register():
-    pass
+def register(request):
+
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+  
+        if password1==password2:
+            user= User.objects.create_user(username=username,password1=password1,email=email,first_name=first_name,last_name=last_name)
+            user.save()
+            print("User Created!")
+
+        else:
+            print("Password Not Matching")
+            return redirect("/")
+
+   
+
+    else:
+        return render(request,'register.html')
